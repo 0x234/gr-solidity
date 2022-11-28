d2982ef3371ea4a764f31d040cb0ba19c70375fe
Jake <jakebunce@gmail.com>
2022-11-28 16:21:03 +0700

    feat: Add example vulnerable contract

---
+++ contracts/myContract.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract MyContract is Initializable {
+    uint256 public x;
+
+    function initialize(uint256 _x) external {
+        x = _x;
+    }
+}
